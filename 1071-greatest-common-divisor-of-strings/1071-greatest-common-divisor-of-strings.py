14b1a864f9056145dec81a4969e3d126f932f849
bheky.dev <127692782+Bh3ky@users.noreply.github.com>
2025-12-05 11:14:06 +0200

    Time: 0 ms (100%), Space: 12.5 MB (19.6%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+
+        def gcd_custom(a, b):
+            while b != 0:
+                a, b = b, a % b
+            return a
+
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        g = gcd_custom(len(str1), len(str2))
+
+        return str1[:g]
+ 
+        
